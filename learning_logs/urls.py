f9a6768a54b733f246c92e4e123e04870ee67d46
jenis980 <jenis.dangol@gmail.com>
2025-09-24 09:05:23 +0545

    feat: add new topic creation form and view
    - Create TopicForm in forms.py for adding new topics
    - Add new_topic URL path in urls.py
    - Implement new_topic view in views.py to handle form submission and redirect to topics list
    
    This enables users to create new topics through a web form, enhancing the app's functionality.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
 
   # Detail page for single topic
   path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+  # page for adding a new topic
+  path('new_topic/', views.new_topic, name='new_topic'),
 ]
