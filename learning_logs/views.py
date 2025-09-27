78bb40b06dda8ba0570ce6edb9f0429fcfe07e68
jenis980 <jenis.dangol@gmail.com>
2025-09-27 21:01:51 +0545

    adding edit template, respective url, views, model and linked with

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -36,3 +36,39 @@ def new_topic(request):
       return HttpResponseRedirect(reverse('learning_logs:topics'))
   context = {'form': form}
   return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+  """Add new entry for a particualr topic."""
+  topic = Topic.objects.get(id=topic_id)
+
+  if request.method != 'POST':
+    # no data submitted; process data.
+    form = EntryForm()
+  else:
+    form = EntryForm(data=request.POST)
+    if form.is_valid():
+      new_entry = form.save(commit=False)
+      new_entry.topic = topic
+      new_entry.save()
+      return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))  
+    
+  context = {'topic': topic, 'form': form}
+  return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+  """Edit an existing entry."""
+  entry = Entry.objects.get(id=entry_id)
+  topic = entry.topic
+
+  if request.method != "POST":
+    # initial request; pre-fill with the current entry.
+    form = EntryForm(instance=entry)
+  else:
+    # POST data submitted; process data.
+    form = EntryForm(instance=entry, data=request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+    
+  context = {'entry': entry, 'topic':topic, 'form': form}
+  return render(request, 'learning_logs/edit_entry.html', context)
